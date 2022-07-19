410330f991294724db761b3f2bb553f52a496b03
legalizemath <89911237+legalizemath@users.noreply.github.com>
2022-07-19 01:00:08 -0400

    qrcodes & amounts & errors

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import bos from './bos.js'
 import fs from 'fs'
 import { bech32 } from 'balanceofsatoshis/node_modules/bech32/dist/index.js'
 import { addAttachedOnion } from './tor.js'
+import QRCode from 'qrcode'
+
 const app = express()
 
 // where to setup local network web server
@@ -13,6 +15,8 @@ const LOCAL_SERVER_PORT = 7890
 const LOCAL_SERVER_ADDRESS = '0.0.0.0'
 const USER_NAME = 'btc' // give username to use for lnurl and lightning address
 const TEXT_MESSAGE = `Send sats. ${USER_NAME}` // text to show senders
+const MAX_SENDABLE = 21e5 * 1e3 // max msats can send
+const MIN_SENDABLE = 1e3 // min msats can send
 
 // how to access tor service controller and which port to use for new .onion
 const { controlPassword, hsPrivateKey } = JSON.parse(fs.readFileSync('./settings.json'))
@@ -38,28 +42,41 @@ const run = async () => {
 
   const url_root = `${torRes.serviceId}.onion`
   const lnurl_utf8 = `http://${url_root}/.wellknown/lnurlp/${USER_NAME}`
-  const lnurlp = bech32.encode('lnurl', bech32.toWords(Buffer.from(lnurl_utf8, 'utf8')), 2000)
+  const lnurlp = bech32.encode('lnurl', bech32.toWords(Buffer.from(lnurl_utf8, 'utf8')), 2000).toUpperCase()
   const lightningAddress = `${USER_NAME}@${url_root}`
 
-  infoText = `resources available at: <br><br>
-    url: ${lnurl_utf8} add &amount=123 to charge specific amount <br><br>
-    lnurlp: ${lnurlp} (works on SWB) <br><br>
-    lightningAddress: ${lightningAddress} (doesn't work anywhere yet) <br><br>
-  `
-  console.log(infoText)
+  infoText = `
+  msats max sendable: ${MAX_SENDABLE}<br>
+  msats min sendable: ${MIN_SENDABLE}<br>
+
+  resources available at: <br><br>
+
+  homepage: ${url_root}
+
+  url: ${lnurl_utf8} <br><br>
+
+  lnurlp: ${lnurlp} <br>
+    (works on SWB) <br><br>
+
+  lightningAddress: ${lightningAddress} <br>
+    (doesn't work anywhere yet) <br><br>
+`
+
+  console.log(infoText.replace(/<br>/g, ''))
+  console.log(await QRCode.toString(lnurlp, { type: 'terminal', small: true }))
+
+  infoText += await QRCode.toString(lnurlp, { type: 'svg', margin: 4, scale: 1, width: 320 })
 
   // backup private key into settings.json so it's re-used next run
-  fs.writeFileSync(
-    './settings.json',
-    JSON.stringify(
-      {
-        controlPassword,
-        hsPrivateKey: torRes.hsPrivateKey
-      },
-      null,
-      2
-    )
-  )
+  // leaking tor hidden service private key can let anyone else generate your onion address
+  const settingsBackup = {
+    controlPassword,
+    hsPrivateKey: torRes.hsPrivateKey,
+    url_root: url_root,
+    lnurl_utf8: lnurl_utf8,
+    lnurlp: lnurlp
+  }
+  fs.writeFileSync('./settings.json', JSON.stringify(settingsBackup, null, 2))
 }
 
 // ---------- set up local webserver ----------
@@ -67,6 +84,7 @@ const run = async () => {
 // lnurl pay path for lightning address
 app.get([`/.wellknown/lnurlp/${USER_NAME}`, `/.wellknown/lnurlp/${USER_NAME}@*`], async (req, res) => {
   // const username = req.params.username // if :username used in get so anything works
+  if (!lnd) return res.status(500).json({ status: 'ERROR', reason: 'LN node not ready yet. Try again later.' })
 
   console.log(`\nget request received for /.wellknown/lnurlp/${USER_NAME}\n`)
 
@@ -80,40 +98,57 @@ app.get([`/.wellknown/lnurlp/${USER_NAME}`, `/.wellknown/lnurlp/${USER_NAME}@*`]
   const msat = req?.query?.amount
 
   const hash = crypto.createHash('sha256').update(JSON.stringify(metadata)).digest('hex')
+  const SENDABLE_ERROR = `Amount ${msat} outside sendable msat range: ${MIN_SENDABLE}-${MAX_SENDABLE}`
 
-  if (msat) {
-    const invoice = await bos.lnService.createInvoice({
-      mtokens: String(msat),
-      description_hash: hash,
-      lnd
-    })
+  try {
+    if (msat) {
+      console.log(`Received request for ${msat} msat payment`)
+      if (+msat > MAX_SENDABLE || +msat < MIN_SENDABLE) throw new Error(SENDABLE_ERROR)
 
-    console.log(`\nnew payment request: ${invoice.request}\n`)
-    // decoded request
-    console.log(
-      'decoded payment request:',
-      bos.lnService.parsePaymentRequest({
-        request: invoice.request
+      const invoice = await bos.lnService.createInvoice({
+        mtokens: String(msat),
+        description_hash: hash,
+        lnd
       })
-    )
 
-    return res.status(200).json({
-      status: 'OK',
-      successAction: { tag: 'message', message: 'Thank You!' },
-      routes: [],
-      pr: invoice.request,
-      disposable: false
-    })
+      console.log(`\nnew payment request: ${invoice.request}\n`)
+      // decoded request
+      console.log(
+        'decoded payment request:',
+        bos.lnService.parsePaymentRequest({
+          request: invoice.request
+        })
+      )
+
+      return res.status(200).json({
+        status: 'OK',
+        successAction: { tag: 'message', message: 'Thank You!' },
+        routes: [],
+        pr: invoice.request,
+        disposable: false
+      })
+    }
+  } catch (e) {
+    console.log('error:', e.message)
+    if (e.message === SENDABLE_ERROR) {
+      res.status(500).json({ status: 'ERROR', reason: SENDABLE_ERROR })
+      return null
+    }
+    // otherwise it's likely a problem with lnd auth so refreshing it
+    res.status(500).json({ status: 'ERROR', reason: 'Error generating invoice. Try again later.' })
+    lnd = undefined
+    lnd = await bos.initializeAuth()
+    return null
   }
 
-  console.log('amountless request')
+  console.log('Received amountless request')
 
   return res.status(200).json({
     status: 'OK',
     callback: callback,
     tag: 'payRequest',
-    maxSendable: 250000000,
-    minSendable: 1000,
+    maxSendable: MAX_SENDABLE,
+    minSendable: MIN_SENDABLE,
     metadata: JSON.stringify(metadata),
     commentsAllowed: 160
   })
